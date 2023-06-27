aad0d1a2dee2d8cc6d08e18da791479c99a280f7
AlekseyMegliy <meglij46@gmail.com>
2023-06-27 17:41:59 +0300

    page 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,26 @@
 import React from "react";
+import Image from "next/image";
+import errorImg from "../../public/error_img.png";
 
 export default function Custom404() {
   return (
-    <div>
-      Якщо ви бачите це повідомлення - це означає, що це та сама помилка 404 -
-      сторінку не знайдено, напишіть нам будь ласка і ми виправимо все якомога
-      швидше, щоб Ви і інші користувачі скоріше змогли зайти на неї
+    <div className="my-8 flex justify-center">
+      <div className="flex w-1/3 flex-col justify-center">
+        <p className="text-ml font-medium">
+          Йой! Сталась халепа! <br />
+          Напишіть нам і ми швидко виправимо проблему
+          <br />
+          Наш{" "}
+          <a
+            className="text-medium font-light hover:text-sky-300"
+            href="https://t.me/alex_NMT_2023"
+          >
+            Telegram
+          </a>{" "}
+          для зв'язку
+        </p>
+      </div>
+      <Image src={errorImg} alt={"Error Image"} className={"w-1/3"} />
     </div>
   );
 }
