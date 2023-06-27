7b60354779cf19031130797c38ae4574080f7e8b
AlekseyMegliy <meglij46@gmail.com>
2023-06-27 11:16:10 +0300

    experiment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,21 @@ import "@/components/styles/globals.css";
 import type { AppProps } from "next/app";
 import Header from "../components/header/header";
 import Footer from "../components/footer/footer";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  weight: ["400", "500"],
+  style: ["normal"],
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <main className={`${montserrat.variable} font-montserrat`}>
       <Header />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </main>
   );
 }
