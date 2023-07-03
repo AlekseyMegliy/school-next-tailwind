38f49187a8decfb7d3c6fc9ec76e280c77436bc9
AlekseyMegliy <meglij46@gmail.com>
2023-07-03 13:25:50 +0300

    versel sendgrid fix 1

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -2,32 +2,32 @@ const mail = require("@sendgrid/mail");
 mail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default async (req, res) => {
-  try {
-    const body = JSON.parse(req.body);
-    const message = `
+  // try {
+  const body = JSON.parse(req.body);
+  const message = `
       Ім'я: ${body.name}\r\n
       Телефон: ${body.phone}\r\n
       Послуга: ${body.service}
     `;
-    const data = {
-      to: "alex.school.ua@gmail.com",
-      from: "alex.school.ua@gmail.com",
-      subject: `Нове повідомлення з сайту від ${body.name}`,
-      text: message,
-      html: message.replace(/\r\n/g, "<br />"),
-    };
-    await mail
-      .send(data)
-      .then(() => {
-        console.log("Email sent");
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+  const data = {
+    to: "alex.school.ua@gmail.com",
+    from: "alex.school.ua@gmail.com",
+    subject: `Нове повідомлення з сайту від ${body.name}`,
+    text: message,
+    html: message.replace(/\r\n/g, "<br />"),
+  };
+  await mail
+    .send(data)
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 
-    res.status(200).json({ status: "OK" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "An error occurred" });
-  }
+  res.status(200).json({ status: "OK" });
+  // } catch (error) {
+  //   console.error(error);
+  //   res.status(500).json({ error: "An error occurred" });
+  // }
 };
